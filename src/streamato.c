e45a344d928ea6429ccaa3bbe716b622487b46e1
Bruno Alano <bruno@appzlab.com>
2015-04-15 00:33:16 -0300

    Base structure

---
+++ src/streamato.c
@@ -0,0 +1,32 @@
+/*
+ * Streamato Movie Streaming
+ * Copyright (C) 2015  Bruno Alano Medina
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Main Block
+ *
+ * This block needs to create the webserver, and
+ * starts the daemon listening to the current directory.
+ * 
+ * @param  argc number of args
+ * @param  argv pointer to array of args
+ * @return      operating system return value
+ */
+int main(int argc, char * argv[])
+{
+  return 0;
+}
